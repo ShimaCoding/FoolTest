0c3ad6f964daf5b090f13f7d9fdb6ece2a8bc0fc
ShimaCoding <shimacoding@gmail.com>
2021-04-15 14:48:00 -0400

    Codigo Random
    owo

---
+++ FoolTest/Assets/owo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class owo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ElPolloSeHaceKK()
+    {
+        Debug.Log("Me hago kk");
+    }
+}
